f68d409504ca8a770c6b1548c4119a7831ddafff
ShruthiBommannan <41140148+ShruthiBommannan@users.noreply.github.com>
2018-07-12 02:32:25 -0700

    Update arr.py

--- arr.py
+++ arr.py
@@ -1,10 +1,8 @@
-n=int(input())
-k=int(input())
-a=[]
-for i in range(1,n+1):
-    a.append(i)
-
-l=0
-for i in range(0,k):
-    l=l+a[i]
-print(l)
+a=input()
+b=input()
+a=a.split()
+b=b.split()
+sum=0
+for i in range(0,int(a[1])):
+    sum=sum+int(b[i])
+print(sum)
